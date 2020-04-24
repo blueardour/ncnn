8dc43f6cf335c0240322e1931390e1fa77a0d542
chenp <chenp@own>
2020-04-24 21:37:09 +0930

    add fcos.cpp

--- examples/fcos.cpp
+++ examples/fcos.cpp
@@ -1,6 +1,9 @@
 
 #include <stdio.h>
+#include <stdlib.h>
 #include <vector>
+#include <queue>
+#include <math.h>
 #include <opencv2/core/core.hpp>
 #include <opencv2/highgui/highgui.hpp>
 #include <opencv2/imgproc/imgproc.hpp>
@@ -8,30 +11,28 @@
 #include "mat.h"
 #include "net.h"
 
-enum {
-  CHANNEL,
-  HEIGHT,
-  WIDTH
-};
-
 int sigmoid(ncnn::Mat &in, ncnn::Mat &out) {
-  int i;
-  float tmp;
-  for(i=0; i<in.w * in.h * in.c; i++) {
-    tmp = -in[i];
-    tmp = exp(tmp) + 1.0f;
-    tmp = 1.0f / tmp;
-    out[i] = tmp;
+  int i, j;
+  float tmp, *cptr1, *cptr2;
+  for(i=0; i<in.c; i++) {
+    cptr1 = in.channel(i);
+    cptr2 = out.channel(i);
+    for(j=0; j<in.w * in.h; j++) {
+      tmp = -cptr1[j];
+      tmp = exp(tmp) + 1.0f;
+      tmp = 1.0f / tmp;
+      cptr2[j] = tmp;
+    }
   }
   return 0;
 }
 
 int multiply(ncnn::Mat &in, ncnn::Mat &in2, ncnn::Mat &out) {
-  int i,j;
+  int i, j;
   float *ptr1, *ptr2, *ptr3;
   for(i=0; i<in.c; i++) {
     ptr1 = in.channel(i);
-    ptr2 = in1.channel(i % in2.c);
+    ptr2 = in2.channel(i % in2.c);
     ptr3 = out.channel(i);
     for(j=0; j<in.h*in.w; j++) {
       ptr3[j] = ptr1[j] * ptr2[j % (in2.w * in2.h)];
@@ -41,30 +42,185 @@ int multiply(ncnn::Mat &in, ncnn::Mat &in2, ncnn::Mat &out) {
 }
 
 int threshold(ncnn::Mat &in, float threshold, ncnn::Mat &out) {
-  int i;
-  for(i=0; i<in.w * in.h * in.c; i++) {
-    out[i] = in[i] > threshold ? 1.0f : 0.0f;
+  int i, j;
+  float *ptr1, *ptr2;
+  for(i=0; i<in.c; i++) {
+    ptr1 = in.channel(i);
+    ptr2 = out.channel(i);
+    for(j=0; j<in.h*in.w; j++)
+      ptr2[j] = ptr1[j] > threshold ? 1.0f : 0.0f;
   }
   return 0;
 }
 
-int sum(ncnn::Mat &in) {
-  int i;
-  float ret = 0.0f;
-  for(i=0; i<in.w * in.h * in.c; i++) {
-    ret += in[i];
+int topk(ncnn::Mat &in, ncnn::Mat &valid_indices, std::vector<std::pair<int, int>> &topk_indices, int limitation) {
+  int i, j;
+  typedef struct Record {
+    int channel, spatial;
+    float value;
+
+    Record() { }
+    Record(int i, int j, float val) {
+      channel = i;
+      spatial = j;
+      value = val;
+    }
+    bool operator < (const struct Record &item) const {
+      return value > item.value;
+    }
+    bool operator < (float val) const {
+      return value < val;
+    }
+  } Record;
+
+  std::priority_queue <Record> heap;
+  float *cptr1, *cptr2;
+  for(i=0; i<in.c; i++) {
+    cptr1 = in.channel(i);
+    cptr2 = valid_indices.channel(i);
+    for(j=0; j<in.w * in.h; j++) {
+      if(cptr2[j] > 0.1f)
+        if(heap.size() < limitation)
+          heap.push(Record(i, j, cptr1[j]));
+        else if(heap.top() < cptr1[j]) {
+          heap.pop();
+          heap.push(Record(i, j, cptr1[j]));
+        }
+    }
   }
-  return (int)ret;
+
+  while(! heap.empty()) {
+    Record item = heap.top();
+    heap.pop();
+    topk_indices.push_back(std::pair<int, int>(item.channel, item.spatial));
+  }
+  return 0;
 }
 
-int postprocess(ncnn::Mat *centerness, ncnn::Mat *bbox_reg, ncnn::Mat *logits, int level=5,
+typedef struct BBox {
+  float box[4];
+  float score;
+  int label;
+  
+  float area() {
+    float width, height, ret;
+    width = box[0] - box[2];
+    height = box[1] - box[3];
+    ret = width * height;
+    if(ret < 0.0f) ret = -ret;
+    return ret;
+  }
+  void norm() {
+    float swap;
+    if(box[0] > box[2]) {
+      swap = box[0];
+      box[0] = box[2];
+      box[2] = swap;
+    } 
+
+    if(box[1] > box[3]) {
+      swap = box[1];
+      box[1] = box[3];
+      box[3] = swap;
+    }
+  }
+
+  float iou(struct BBox &item) {
+    float xA, yA, xB, yB, width, height, iou;
+    xA = item.box[0] > box[0] ? item.box[0] : box[0];
+    yA = item.box[1] > box[1] ? item.box[1] : box[1];
+    xB = item.box[2] < box[2] ? item.box[2] : box[2];
+    yB = item.box[3] < box[3] ? item.box[3] : box[3];
+    width = xB > xA + 1 ? xB - xA + 1: 0;
+    height = yB > yA + 1? yB - yA + 1: 0;
+    iou = width * height;
+    iou = iou / (area() + item.area() - iou);
+    return iou;
+  }
+
+  bool operator < (const struct BBox &item) const {
+    return item.score < score;
+  }
+
+  void print() {
+    fprintf(stdout, "Bounding box: position(%d %d %d %d) in category %d with score %.3f\n", 
+        (int)box[0], (int)box[1], (int)box[2], (int)box[3], label, score);
+  }
+
+} BBox;
+
+int nms_sorted_bboxes(std::vector<BBox> &boxlist, std::vector<int> &picked, float nms_threshold) {
+  int i, j;
+
+  picked.clear();
+  for (i=0; i<(int)boxlist.size(); i++) {
+    boxlist[i].norm();
+  }
+
+  bool keep;
+  float iou;
+  for (i=0; i<(int)boxlist.size(); i++) {
+    keep = true;
+    for (j=0; j<(int)picked.size(); j++) {
+      iou = boxlist[i].iou(boxlist[picked[j]]);
+      if(iou > nms_threshold) keep = false;
+    }
+    if (keep) picked.push_back(i);
+  }
+  return 0;
+}
+
+int ml_nms(std::vector<BBox> &boxlist, std::vector<int> &keep, float nms_thresh) {
+  int i;
+
+  // shift box according to label to seperate different categories
+  int max, min;
+  max = min = 0;
+  for(i=0; i<boxlist.size(); i++) {
+    max = max < boxlist[i].box[0] ? boxlist[i].box[0] : max;
+    max = max < boxlist[i].box[1] ? boxlist[i].box[1] : max;
+    max = max < boxlist[i].box[2] ? boxlist[i].box[2] : max;
+    max = max < boxlist[i].box[3] ? boxlist[i].box[3] : max;
+  }
+  max = max + 1;
+
+  for(i=0; i<boxlist.size(); i++) {
+    boxlist[i].box[0] += boxlist[i].label * max;
+    boxlist[i].box[1] += boxlist[i].label * max;
+    boxlist[i].box[2] += boxlist[i].label * max;
+    boxlist[i].box[3] += boxlist[i].label * max;
+  }
+
+  // sort
+  std::sort(boxlist.begin(), boxlist.end());
+
+  // nms
+  nms_sorted_bboxes(boxlist, keep, nms_thresh);
+
+  // shift box back
+  for(i=0; i<keep.size(); i++) {
+    boxlist[keep[i]].box[0] -= boxlist[i].label * max;
+    boxlist[keep[i]].box[1] -= boxlist[i].label * max;
+    boxlist[keep[i]].box[2] -= boxlist[i].label * max;
+    boxlist[keep[i]].box[3] -= boxlist[i].label * max;
+  }
+  return 0;
+}
+
+int postprocess(std::vector <BBox> &detection, ncnn::Mat *centerness, ncnn::Mat *bbox_reg, ncnn::Mat *logits, std::vector <int> &strides,
     bool thresh_with_ctr=false, float pre_nms_thresh=0.5f, int pre_nms_top_n=1000, float nms_thresh=0.6f, int post_nms_top_n=100)
 {
-  int ret = 0;
-  int l;
-  int height, width, channel;
-  int pre_top_n;
-  for(l=0; l<level; l++) {
+  int l, i;
+  int channel, height, width, stride;
+  float *cptr;
+  std::vector <BBox> boxlist;
+  BBox box;
+  for(l=0; l<strides.size(); l++) {
+    stride = strides[l];
+    channel = logits[l].c;
+    height = logits[l].h;
+    width = logits[l].w;
+
     sigmoid(centerness[l], centerness[l]);
     sigmoid(logits[l], logits[l]);
 
@@ -72,17 +228,41 @@ int postprocess(ncnn::Mat *centerness, ncnn::Mat *bbox_reg, ncnn::Mat *logits, i
       multiply(logits[l], centerness[l], logits[l]);
     }
 
-    ncnn::Mat candidate_inds(logits[l].w, logits[l].h, logits[l].c);
+    ncnn::Mat candidate_inds(width, height, channel);
     threshold(logits[l], pre_nms_thresh, candidate_inds);
-    pre_top_n = sum(candidate_inds);
-    pre_top_n = pre_top_n > pre_nms_top_n ? pre_nms_top_n : pre_top_n;
 
     if(! thresh_with_ctr) {
       multiply(logits[l], centerness[l], logits[l]);
     }
 
+    std::vector <std::pair <int, int>> indices;
+    topk(logits[l], candidate_inds, indices, pre_nms_top_n);
+
+    for(i=0; i<indices.size(); i++) {
+      cptr = logits[l].channel(indices[i].first);
+      box.score = sqrt(cptr[indices[i].second]);
+      box.label = indices[i].first;
+      cptr = bbox_reg[l].channel(0);
+      box.box[0] = (indices[i].second % width)*stride + stride/2 - cptr[indices[i].second]*stride;
+      cptr = bbox_reg[l].channel(1);
+      box.box[1] = (indices[i].second / width)*stride + stride/2 - cptr[indices[i].second]*stride;
+      cptr = bbox_reg[l].channel(2);
+      box.box[2] = (indices[i].second % width)*stride + stride/2 + cptr[indices[i].second]*stride;
+      cptr = bbox_reg[l].channel(3);
+      box.box[3] = (indices[i].second / width)*stride + stride/2 + cptr[indices[i].second]*stride;
+      boxlist.push_back(box);
+    }
   }
-  return ret;
+
+  std::vector <int> keep;
+  ml_nms(boxlist, keep, nms_thresh);
+
+  // neglect filter by post_nms_top_n
+  
+  // save result
+  for(i=0; i<keep.size(); i++)
+    detection.push_back(boxlist[keep[i]]);
+  return 0;
 }
 
 int demo(ncnn::Net &net, char *fname, int h, int w) {
@@ -99,7 +279,7 @@ int demo(ncnn::Net &net, char *fname, int h, int w) {
 
   // forward
   ncnn::Extractor ex = net.create_extractor();
-  ex.set_num_threads(4);
+  //ex.set_num_threads(4);
 
   // input
   ex.input("input_image", input);
@@ -107,23 +287,31 @@ int demo(ncnn::Net &net, char *fname, int h, int w) {
   ncnn::Mat centerness[5];
   ncnn::Mat bbox_reg[5];
   ncnn::Mat logits[5];
-  ex.extract("P3_centerness", centerness[0]);
-  ex.extract("P4_centerness", centerness[1]);
-  ex.extract("P5_centerness", centerness[2]);
-  ex.extract("P6_centerness", centerness[3]);
-  ex.extract("P7_centerness", centerness[4]);
-  ex.extract("P3_bbox_reg", bbox_reg[0]);
-  ex.extract("P4_bbox_reg", bbox_reg[1]);
-  ex.extract("P5_bbox_reg", bbox_reg[2]);
-  ex.extract("P6_bbox_reg", bbox_reg[3]);
-  ex.extract("P7_bbox_reg", bbox_reg[4]);
-  ex.extract("P3_logits", logits[0]);
-  ex.extract("P4_logits", logits[1]);
-  ex.extract("P5_logits", logits[2]);
-  ex.extract("P6_logits", logits[3]);
-  ex.extract("P7_logits", logits[4]);
+  ex.extract("P3centerness", centerness[0]);
+  ex.extract("P4centerness", centerness[1]);
+  ex.extract("P5centerness", centerness[2]);
+  ex.extract("P6centerness", centerness[3]);
+  ex.extract("P7centerness", centerness[4]);
+  ex.extract("P3bbox_reg", bbox_reg[0]);
+  ex.extract("P4bbox_reg", bbox_reg[1]);
+  ex.extract("P5bbox_reg", bbox_reg[2]);
+  ex.extract("P6bbox_reg", bbox_reg[3]);
+  ex.extract("P7bbox_reg", bbox_reg[4]);
+  ex.extract("P3logits", logits[0]);
+  ex.extract("P4logits", logits[1]);
+  ex.extract("P5logits", logits[2]);
+  ex.extract("P6logits", logits[3]);
+  ex.extract("P7logits", logits[4]);
 
-  postprocess(centerness, bbox_reg, logits);
+  std::vector <int> strides { 8, 16, 32, 64, 128 };
+  std::vector <BBox> detection;
+  postprocess(detection, centerness, bbox_reg, logits, strides);
+
+  // print result
+  for(int i=0; i<detection.size(); i++) {
+    detection[i].print();
+  }
+
   return 0;
 }
 
